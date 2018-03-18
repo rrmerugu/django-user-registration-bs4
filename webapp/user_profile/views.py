d33b6497e304d716527cc474247620e84705b18d
Ravi RT Merugu <rrmerugu@gmail.com>
2018-03-18 13:19:45 +0530

    user profile ui changes

--- webapp/user_profile/views.py
+++ webapp/user_profile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
+from django.shortcuts import redirect, reverse
 from user_profile.forms import UserProfileModelForm, UserDetailModelForm
 
 
@@ -13,18 +14,17 @@ class UserProfileView(TemplateView, LoginRequiredMixin):
 
     def post(self, request, *args, **kwargs):
         context = super(UserProfileView, self).get_context_data(**kwargs)
-        context['user_profile_form'] = user_profile_form = UserProfileModelForm(request.POST,
+        context['user_profile_form'] = user_profile_form = UserProfileModelForm(request.POST, request.FILES,
                                                                                 instance=request.user.user_profile)
-        context['user_detail_form'] = user_detail_form = UserDetailModelForm(request.POST, instance=request.user)
-        if user_profile_form.is_valid():
+        context['user_detail_form'] = user_detail_form = UserDetailModelForm(request.POST,
+                                                                             instance=request.user)
+        if user_profile_form.is_valid() and user_detail_form.is_valid():
             user_profile_form.save()
+            user_detail_form.save()
+            return redirect(reverse('profile'))
         else:
             print(user_profile_form.errors, "++++++")
 
-        if user_detail_form.is_valid():
-            user_detail_form.save()
-        else:
-            print(user_detail_form.errors, "-------")
         return render(request, self.template_name, context=context)
 
     def get(self, request, *args, **kwargs):
