23c79a945ab9f59393c21633ce073d0c1d0031e2
Ravi RT Merugu <rrmerugu@gmail.com>
2018-03-18 12:44:35 +0530

    terms page added

--- webapp/webpages/urls.py
+++ webapp/webpages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name="homepage"),
+    url(r'^terms$', views.TermsView.as_view(), name="terms"),
 ]
--- webapp/webpages/views.py
+++ webapp/webpages/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "webpages/homepage.html"
+
+class TermsView(TemplateView):
+    template_name = "webpages/terms.html"
