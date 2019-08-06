59db7e18e297121bebc07598a2dc3be75e62bfde
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-08-06 15:39:19 +0530

    requirements updated

--- webapp/example_webapp/settings.py
+++ webapp/example_webapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '0x9%f5$w(79=-9k*=g_90!)p(rvo4wh)tn)0vrozsscbh5lj2b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
